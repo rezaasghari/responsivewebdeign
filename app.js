4db035d98422380e5374e11fabf5f0ce556aff6f
Reza Asghari <reza_asghari83@yahoo.com>
2021-06-04 02:01:00 +0430

    worked on css and changed some features js file is not complete yet

---
+++ app.js
@@ -0,0 +1,8 @@
+$ (document).ready(function){
+    // MOBILE NAVIGATION
+    $(".burger-icon").on("click", function(){
+        if ($("main-nav").hasClass("mobile-hide")){
+            
+        }
+    })
+}
